1535a360e86b7b05618a5a5dafb0ce49fbc953d2
Kamika-ZE <krysnn@gmail.com>
2020-05-20 23:01:34 +0200

    initial commit

---
+++ src/main/java/org/mickael/libraryauthserver/LibraryAuthServerApplication.java
@@ -0,0 +1,13 @@
+package org.mickael.libraryauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAuthServerApplication.class, args);
+	}
+
+}
